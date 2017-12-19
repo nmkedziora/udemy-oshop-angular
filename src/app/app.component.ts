b413e10a2c44581976978306cee91538ced6163f
Natalia Kędziora <n.m.kedziora@gmail.com>
2017-12-19 14:23:21 +0100

    Implements redirection after loggin in, to ensure better UX

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  constructor(private angularFireAuth: AngularFireAuth, private router: Router) {
+    this.angularFireAuth.auth.getRedirectResult().then(result => {
+      if (result.user) {
+        let route = localStorage.getItem('returnUrl');
+
+        this.router.navigate([route]);
+      }
+    });
+  }
+
+  //   alternatively:
+
+  //   constructor(private auth: AuthService, private router: Router) {
+  //   auth.user$.subscribe(user => {
+  //     if (user) {
+  //       let route = localStorage.getItem('returnUrl');
+
+  //       this.router.navigateByUrl(route);
+  //     }
+  //   });
+  // }
 }
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase';
 import { Observable } from 'rxjs/Observable'; 
@@ -7,11 +8,14 @@ import { Observable } from 'rxjs/Observable';
 export class AuthService {
   user$: Observable<firebase.User>;
 
-  constructor(private angularFireAuth: AngularFireAuth) {
+  constructor(private angularFireAuth: AngularFireAuth, private route: ActivatedRoute) {
     this.user$ = angularFireAuth.authState;  
   }
 
   login() {
+    let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
+
+    localStorage.setItem('returnUrl', returnUrl);
     this.angularFireAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
   }
 
