6d92e38f254a4a1dd0f610929ebcdfc529719871
Natalia Kędziora <n.m.kedziora@gmail.com>
2017-12-19 11:54:29 +0100

    Adds async pipe to automatically unsubscribe from Firebase observable

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'bs-navbar',
@@ -10,10 +11,10 @@ import * as firebase from 'firebase';
 
 export class BsNavbarComponent {
   show: boolean = false;
-  user: firebase.User;
+  user$: Observable<firebase.User>;
 
   constructor(private afAuth: AngularFireAuth) {
-    afAuth.authState.subscribe(user => this.user = user);
+    this.user$ = afAuth.authState;
     
   }
 
