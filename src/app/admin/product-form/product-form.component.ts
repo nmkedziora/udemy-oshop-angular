bf1f81685b9ca60d9dc9c846fdfa1177ce56010e
Natalia Kedziora <natalia.kedziora@softwareplant.com>
2017-12-30 18:06:30 +0100

    Adds Bootstrap card to preview product while adding

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CategoryService } from '../../category.service';
 import { ProductService } from '../../product.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-form',
@@ -10,11 +11,15 @@ import { ProductService } from '../../product.service';
 export class ProductFormComponent {
   categories$;
 
-  constructor(categoryService: CategoryService, private productService: ProductService) {
+  constructor(
+    categoryService: CategoryService,
+    private productService: ProductService
+    private router: Router) {
     this.categories$ = categoryService.getCategories();
   }
 
   save(product) {
     this.productService.create(product);
+    this.router.navigate(['/admin/products'])
   }
 }
