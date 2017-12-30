b9b155121443a74a0d9157dd2d1ed82a3c0cbd07
Natalia Kedziora <natalia.kedziora@softwareplant.com>
2017-12-30 19:51:49 +0100

    Implements products filtering on client side

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { ProductService } from '../../product.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent {
-  products$;
+export class AdminProductsComponent implements OnDestroy {
+  products: any[];
+  filtered: any[];
+  subscription: Subscription
 
   constructor(private productService: ProductService) {
-    this.products$ = productService.getAll();
+    this.subscription = this.productService.getAll().subscribe(products => this.products = this.filtered = products);
+  }
+
+  filter(query: string) {
+    this.filtered = query ?
+      this.products.filter(product => product.title.toLowerCase().includes(query.toLowerCase())) :
+      this.products;
+  }
+
+
+  ngOnDestroy () {
+    this.subscription.unsubscribe();
   }
 }
